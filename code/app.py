74416b0e1c0851f014ac4e83946279fb8b4bf9e1
1999azzar <azzar.mr.zs@gmail.com>
2024-05-18 23:22:21 +0700

    dump

---
+++ code/app.py
@@ -0,0 +1,140 @@
+import streamlit as st
+import pyperclip
+from generator import GenerativeModel  # Import the GenerativeModel class
+
+# Set the page configuration
+st.set_page_config(page_title="Prompt Sanctuary", page_icon="./img/favicon.ico")
+
+# Create an instance of the GenerativeModel class
+gen_model = GenerativeModel()
+
+def main():
+    st.sidebar.image("./img/logo.png", use_column_width=False, width=200)
+    st.sidebar.title("Navigation")
+    page = st.sidebar.radio("Go to", ("Text Prompt", "Image Prompt"))
+
+    with st.sidebar:
+        gemini_api_key = st.sidebar.text_input("Gemini API Key", key="chatbot_api_key", type="password")
+        if gemini_api_key:
+            gen_model.set_api_key(gemini_api_key)
+        else:
+            st.sidebar.info("Please enter your Gemini API key to continue.")
+        st.markdown("[Get a gemini API key](https://aistudio.google.com/app/apikey)")
+        st.markdown("[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)")
+        st.markdown("powered by :")
+        st.markdown("[![gemini](https://www.gstatic.com/aistudio/ai_studio_favicon_32x32.svg)](https://aistudio.google.com)")
+
+    if page == "Text Prompt":
+        display_txt()
+    elif page == "Image Prompt":
+        display_img()
+
+def display_txt():
+    st.title("Prompt generator (text)")
+    generated_prompt = None   # Initialize generated_prompt
+    prompt_generated = False  # Initialize prompt_generated flag
+
+    st.markdown(
+        """
+        <div style="text-align: justify">
+        Here, you have the flexibility to create a customized prompt tailored to your specific requirements. 
+        Alternatively, you can opt to use the random feature, which will generate a prompt for you—ensuring not 
+        only variety but also an element of surprise and enjoyment in the process. Whether you have a specific 
+        idea in mind or are seeking inspiration, the Prompt Generator is designed to accommodate both your 
+        structured and spontaneous prompt needs.
+        </div>
+        """,
+        unsafe_allow_html=True  # Allow HTML tags to style the markdown
+    )
+    st.subheader("Generate Text Prompt")
+    user_input_text = st.text_input("Input:", "write a python code")
+    
+    # Use columns layout to display buttons side by side
+    col1, col2 = st.columns(2)
+    
+    with col1:
+        if st.button("Generate", key=hash(user_input_text)):
+            if not st.session_state.get("chatbot_api_key"):
+                st.info("Please add your Gemini API key to continue.")
+                st.stop()
+            generated_prompt = gen_model.generate_response(user_input_text)
+            prompt_generated = True
+    
+    with col2:
+        if st.button("Random", key=hash(f"random")):
+            if not st.session_state.get("chatbot_api_key"):
+                st.info("Please add your Gemini API key to continue.")
+                st.stop()
+            generated_prompt = gen_model.generate_random()
+            prompt_generated = True
+        
+    if generated_prompt is not None:
+        st.write(f"{generated_prompt}")
+        if prompt_generated and st.button("Copy Prompt"):
+            pyperclip.copy(generated_prompt)
+            st.success("Prompt copied to clipboard!")
+
+def display_img():
+    st.title("Prompt generator (img)")
+    generated_prompt = None  # Initialize generated_prompt
+    prompt_generated = False  # Initialize prompt_generated flag
+    txt = False
+    img = False
+    rdm = False
+
+    st.markdown(
+        """
+        <div style="text-align: justify">
+        Here, you have the flexibility to create a customized prompt tailored to your specific requirements. 
+        Alternatively, you can opt to use the random feature, which will generate a prompt for you—ensuring not 
+        only variety but also an element of surprise and enjoyment in the process. Whether you have a specific 
+        idea in mind or are seeking inspiration, the Prompt Generator is designed to accommodate both your 
+        structured and spontaneous prompt needs.
+        </div>
+        """,
+        unsafe_allow_html=True  # Allow HTML tags to style the markdown
+    )
+    st.subheader("Generate Image Prompt")
+    prompt_option = st.radio("Generate from option:", ("Text", "File", "Random"))
+    if prompt_option == "Text":
+        txt = True
+        img = False
+        rdm = False
+        user_input_image = st.text_input("Input:", "whale on the desert")
+    elif prompt_option == "File":
+        txt = False
+        img = True
+        rdm = False
+        image_file = st.file_uploader("Upload Image", type=["jpg", "jpeg", "png"])
+    elif prompt_option == "Random":
+        txt = False
+        img = False
+        rdm = True
+    
+    if txt:
+        if st.button("Generate", key=hash(user_input_image)):
+            if not st.session_state.get("chatbot_api_key"):
+                st.info("Please add your Gemini API key to continue.")
+                st.stop()
+            generated_prompt = gen_model.generate_imgdescription(user_input_image)
+    elif img:
+        if st.button("Generate", key=hash(str(image_file))):
+            if not st.session_state.get("chatbot_api_key"):
+                st.info("Please add your Gemini API key to continue.")
+                st.stop()
+            generated_prompt = gen_model.reverse_image(image_file)
+    elif rdm:
+        if st.button("Generate", key=hash(f"vrandom")):
+            if not st.session_state.get("chatbot_api_key"):
+                st.info("Please add your Gemini API key to continue.")
+                st.stop()
+            generated_prompt = gen_model.generate_vrandom()
+
+    if generated_prompt is not None:
+        st.write(f"{generated_prompt}")
+        if prompt_generated and st.button("Copy Prompt"):
+            pyperclip.copy(generated_prompt)
+            st.success("Prompt copied to clipboard!")
+
+if __name__ == "__main__":
+    main()
---
+++ code/generator.py
@@ -0,0 +1,113 @@
+# generative_model.py
+import google.generativeai as genai
+import random
+
+class GenerativeModel:
+    def __init__(self):
+        self.api_keys = []
+        self.current_key_index = 0
+
+        # Configure the model
+        self.generation_config = {
+            "temperature": 0.75,        # Controls the randomness of generated responses
+            "top_p": 0.65,              # Top-p (nucleus) sampling parameter
+            "top_k": 35,                # Top-k filtering parameter for token sampling
+            "max_output_tokens": 2048,  # Maximum number of tokens in the generated response
+            'stop_sequences': [],       # Sequences to stop generation at
+        }
+
+        # Safety settings
+        self.safety_settings = [
+            {"category": "HARM_CATEGORY_HARASSMENT", "threshold": "BLOCK_NONE"},
+            {"category": "HARM_CATEGORY_HATE_SPEECH", "threshold": "BLOCK_NONE"},
+            {"category": "HARM_CATEGORY_SEXUALLY_EXPLICIT", "threshold": "BLOCK_NONE"},
+            {"category": "HARM_CATEGORY_DANGEROUS_CONTENT", "threshold": "BLOCK_NONE"}
+        ]
+
+        # Initialize the model
+        self.model = genai.GenerativeModel(
+            model_name="gemini-1.5-pro-latest",
+            generation_config=self.generation_config,
+            safety_settings=self.safety_settings,
+        )
+
+    def set_api_key(self, api_key):
+        self.api_keys = api_key.split(',')
+        self.current_key_index = 0
+
+    def get_current_api_key(self):
+        key = self.api_keys[self.current_key_index]
+        self.current_key_index = (self.current_key_index + 1) % len(self.api_keys)
+        return key
+
+    def configure_api_key(self):
+        genai.api_key = self.get_current_api_key()
+
+    def read_prompt_parts_from_file(self, file_path, user_input_text=''):
+        with open(file_path, 'r') as file:
+            prompt_parts = file.read()
+        if user_input_text:
+            prompt_parts = prompt_parts.replace('{user_input_text}', user_input_text)
+        return prompt_parts
+
+    def generate_response(self, user_input_text):
+        self.configure_api_key()
+        prompt_parts = self.read_prompt_parts_from_file('./instruction/examples1.txt', user_input_text)
+        response = self.model.generate_content(prompt_parts)
+        return response.text
+
+    def generate_random(self):
+        self.configure_api_key()
+        prompt_parts = self.read_prompt_parts_from_file('./instruction/examples2.txt')
+        response = self.model.generate_content(prompt_parts)
+        return response.text
+
+    def generate_imgdescription(self, user_input_image):
+        self.configure_api_key()
+        with open('./instruction/image_styles.txt', 'r') as file:
+            image_styles = [line.strip() for line in file.readlines()]
+
+        chosen_styles = random.sample(image_styles, k=3)
+        prompt_parts = [
+            " ",
+            f"Input: Use the following styles ({', '.join(chosen_styles)}) to create a compelling image description about {user_input_image}. if possible Incorporate elements all of those styles into your description. Your narrative should be between 200 to 400 characters, evoking a vivid and imaginative scene. Start your description with the word 'imagine,' e.g., 'imagine a hyperrealistic portrait in a dreamlike landscape...'",
+            "Output: ",
+        ]
+        response = self.model.generate_content(prompt_parts)
+        return response.text
+
+    def generate_vrandom(self):
+        self.configure_api_key()
+        with open('./instruction/image_styles.txt', 'r') as file:
+            image_styles = [line.strip() for line in file.readlines()]
+
+        chosen_styles = random.sample(image_styles, k=3)
+        prompt_parts = [
+            " ",
+            f"Input: Use the following styles ({', '.join(chosen_styles)}) to create a compelling image description. if possible Incorporate elements all of those styles into your description. Your narrative should be between 200 to 1000 characters, evoking a vivid and imaginative scene. Start your description with the word 'imagine,' e.g., 'imagine a hyperrealistic portrait in a dreamlike landscape...'",
+            "Output: ",
+        ]
+        response = self.model.generate_content(prompt_parts)
+        return response.text
+
+    def generate_content(self, prompt_parts):
+        response = self.model.generate_content(prompt_parts)
+        return response.text
+
+    def reverse_image(self, image_data):
+        try:
+            with open('./instruction/image_styles.txt', 'r') as file:
+                image_styles = [line.strip() for line in file.readlines()]
+
+            prompt_parts = [
+                "\nPlease provide a detailed description, written in proper English, to recreate this image in 250 to 500 words. Include information about the style, mood, lighting, and other important details. Ensure your sentences are complete and free from spelling and grammar errors:",
+                {"mime_type": "image/jpeg", "data": image_data},
+                f"\nPlease select and use up to four different artistic styles from the following list: \n{', '.join(image_styles)}\nYou can choose the same style multiple times if desired.",
+                "Try to make your description as similar as possible to the original image, just like an audio describer would. Remember to begin your description with the word 'imagine.' For example, 'imagine a red-hooded woman in the forest...'",
+            ]
+
+            response_text = self.generate_content(prompt_parts)
+            return response_text
+
+        except Exception as e:
+            return f"Error: {str(e)}"
